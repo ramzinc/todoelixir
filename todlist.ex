ac0ac99cf335b130eb1934eb873b641606a2734a
Arnold Mpiima <ramzinc101@gmail.com>
2019-12-14 11:46:34 +0300

    Todolist from importation of csv fields: extract,transform,load
    Signed-off-by: Arnold Mpiima <ramzinc101@gmail.com>

---
+++ todlist.ex
@@ -0,0 +1,86 @@
+defmodule ToDoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(entries, %ToDoList{}, &add_entry(&2, &1))
+  
+  end
+
+  def add_entry(todolist, entry) do
+    #set the id of the entry with that stored in the structure's auto_id field
+    entry = Map.put(entry, :id, todolist.auto_id)
+    #add the entry to the entries collection 
+    new_entries = Map.put(todolist.entries, todolist.auto_id, entry)
+    #Update the todolist struct instance with  the new_entries collection.
+    %ToDoList{todolist |
+	      entries: new_entries,
+	      auto_id: todolist.auto_id + 1
+    }
+  end
+
+  def entries(todolist,date)do
+    entries = todolist.entries |> Stream.filter(fn {_,entry} -> entry.date == date end ) |> Enum.map(fn {_,entry} ->
+      entry
+      end
+    )
+    entries
+
+  end
+
+  def update_entry(todolist, id , update_fun) do
+    case Map.fetch(todolist.entries, id) do
+      :error ->
+	"An error happend"
+	
+      {:ok, old_entry} ->
+	#Make sure we are returning a map 
+	new_entry = %{} = update_fun.(old_entry)
+	new_entries = Map.put(todolist.entries, new_entry.id,new_entry )
+	%ToDoList{todolist | entries: new_entries}
+    end 
+
+  end
+
+  def delete_entry(todolist, id) do
+    entry = Map.delete(todolist.entries, id)
+    %ToDoList{todolist | entries: entry}
+    end
+end
+
+defmodule ToDoList.CsvImporter do
+
+  def open_file(path)do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> separate_dates
+    |> ToDoList.new()
+  end
+
+  def separate_dates(lines) do
+   lines
+   |> Stream.map(&separate_date_from_title/1)
+  end
+
+  def separate_date_from_title(dates_and_title_str) do
+    dates_and_title_str
+    |>IO.inspect
+    |>String.split(",")
+    |>match_dates()
+  end
+
+  def match_dates([datestring,title])do
+    [year,month,date] = String.split(datestring, "/")
+    {_, date_entry} = Date.new(String.to_integer(year), String.to_integer(month), String.to_integer(date))
+    create_entry({date_entry,title})
+  end
+
+  def create_entry({date_e, title})do
+    %{date: date_e, title: title}
+  end
+    
+  
+end
+
+ 
+ToDoList.CsvImporter.open_file("/home/zeus/experiments/elixir/elixir/todolist/todocsv.csv");
